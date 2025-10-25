c8583f26284f893ee363334cff50066d2134d0a2
AbelPalominoRojas <abel.palominorojas@gmail.com>
2025-10-25 09:55:41 -0500

    start project

---
+++ src/main/java/com/ironman/book/ReactiveApiBookApplication.java
@@ -0,0 +1,13 @@
+package com.ironman.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApiBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApiBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironman/book/ReactiveApiBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironman.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApiBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
